9c9985200fb1dbb0e7c54a680beb298b17d1cf31
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-04-12 23:46:24 +0300

    Create SingletonPattern

---
+++ Creational-Patterns/SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    internal class Audi
+    {
+        private static Audi _onlyInstance;
+        
+        private Audi()
+        {
+            
+        }
+        
+        public void StartEngine()
+        {
+            Console.WriteLine("Start engine!");
+        }
+
+        public static Audi GetInstance()
+        {
+            _onlyInstance ??= new Audi();
+
+            return _onlyInstance;
+        }
+    }
+    
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var audi = Audi.GetInstance();
+            audi.StartEngine();
+        }
+    }
+}
