aa55929d85cb870461d037d803624adb3543cc26
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-05-04 21:44:50 +0300

    Create CompositePattern

---
+++ Structural-Patterns/CompositePattern/CompositePattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CompositePattern
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var airforce = new Airforce();
+
+            var f16 = new F16();
+            var f35 = new F35();
+            
+            airforce.Add(f16);
+            airforce.Add(f35);
+            
+            Console.WriteLine(airforce.GetPersonnel());
+        }
+    }
+}
